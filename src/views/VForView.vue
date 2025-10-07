c28e8696e6bdc187ad9dcd44276252d17a02dddc
Miguel Álvarez <alteregoreborn@gmail.com>
2025-10-06 20:18:03 -0600

    First Commit Proyecto

---
+++ src/views/VForView.vue
@@ -0,0 +1,37 @@
+<template>
+  <section>
+    <h2>v-for</h2>
+    <div>
+      <input v-model="nueva" placeholder="Agregar item" />
+      <button @click="agregar">Agregar</button>
+    </div>
+    <ul>
+      <li v-for="(item, i) in lista" :key="i">
+        {{ item }}
+        <button @click="eliminar(i)">Eliminar</button>
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      lista: ['Manzana', 'Banana', 'Cereza'],
+      nueva: ''
+    }
+  },
+  methods: {
+    agregar() {
+      if (this.nueva.trim()) {
+        this.lista.push(this.nueva.trim())
+        this.nueva = ''
+      }
+    },
+    eliminar(i) {
+      this.lista.splice(i, 1)
+    }
+  }
+}
+</script>
---
+++ src/views/VShowView.vue
@@ -0,0 +1,21 @@
+<template>
+  <section>
+    <h2>v-show</h2>
+    <div>
+      <button @click="visible = !visible">{{ visible ? 'Ocultar' : 'Mostrar' }}</button>
+    </div>
+    <div>
+      <p v-show="visible">Texto controlado por v-show</p>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      visible: true
+    }
+  }
+}
+</script>
